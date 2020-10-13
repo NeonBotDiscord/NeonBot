2e20842df5bd4361b9e4edf7cf491fefbb47a938
NeonRichy <blitzproductionsbackup@gmail.com>
2020-10-13 22:44:26 +0100

    Made it so the bot finally works

---
+++ events/message.js
@@ -0,0 +1,16 @@
+module.exports => {
+fs.readdir('./commands/', (err, files) => {
+	if (err) console.log(`${err}`);
+	let jsfile = files.filter((f) => f.split('.').pop() === 'js');
+	if (jsfile.length <= 0) {
+		console.log('Could not find commands.');
+		return;
+	}
+	jsfile.forEach((f, i) => {
+		let props = require(`./commands/${f}`);
+		console.log(`${f} Loaded`);
+		bot.commands.set(props.config.name, props);
+	});
+	console.log("All commands have loaded successfully" .green)
+});
+}
