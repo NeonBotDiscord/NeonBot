9adaba219297818fb94af5071424ee4b51a90872
NeonRichy <blitzproductionsbackup@gmail.com>
2020-10-14 08:07:45 +0100

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,18 +1,23 @@
-const fs = require('fs')
+require('dotenv').config(); //gets dotenv for all the secret stuff
+const log = new discord.WebhookClient(process.env.WEBHOOKID, process.env.WEBHOOKTOKEN); // logging place
 
 module.exports = () => {
-fs.readdir('.../commands/', (err, files) => {
-	if (err) console.log(`${err}`);
-	let jsfile = files.filter((f) => f.split('.').pop() === 'js');
-	if (jsfile.length <= 0) {
-		console.log('Could not find commands.');
-		return;
-	}
-	jsfile.forEach((f, i) => {
-		let props = require(`./commands/${f}`);
-		console.log(`${f} Loaded`);
-		bot.commands.set(props.config.name, props);
-	});
-	console.log("All commands have loaded successfully" .green)
-});
+     if(!message.content.startsWith(mainjson.settings.prefix)) return;
+     if(message.author.bot) return;
+     if(message.channel.type === "DM") return;
+     let ArrayMessage = message.content.slice(mainjson.settings.prefix.length).split(' ')
+     let command = ArrayMessage[ 0 ]
+     let args = ArrayMessage.slice(1)
+     if(!command) return;
+     let commandfile = bot.commands.get(command)
+     if(!commandfile) return;
+     commandfile.run(bot, message, args)
+     let logmsg = `COMMAND | ${message.author.username} (${message.author.id}) | ${command}`
+     console.log(logmsg)
+     let logem = new discord.MessageEmbed()
+     .setTitle("COMMAND USED")
+     .setDescription(logmsg)
+     .setFooter(`${bot.user.username} Logger`)
+     .setColor("GREEN")
+     log.send(logem)
 }
