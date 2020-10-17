7be89be86b46ee1c98779a179a7c8d40cf2083f5
NeonRichy <blitzproductionsbackup@gmail.com>
2020-10-17 17:06:54 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,11 +18,10 @@ global.discord = require('discord.js');
 global.log = new discord.WebhookClient(process.env.WEBHOOKID, process.env.WEBHOOKTOKEN);
 const fs = require('fs');
 global.mongoose = require('mongoose')
-mongoose.connect(process.env.DBSERVERLOGIN,{
+global.db = mongoose.connect(`${process.env.DBSERVERLOGIN}`,{
         useNewUrlParser: true,
         useUnifiedTopology: true
-})
-.then(() => console.log("Mongoose has connected."))
+}).then(() => console.log("Mongoose has connected."))
 .catch(err => console.log(`Mongoose failed to connect. Error: ${err}`))
 const bot = new discord.Client({ disableEveryone: true });
 bot.commands = new discord.Collection();
